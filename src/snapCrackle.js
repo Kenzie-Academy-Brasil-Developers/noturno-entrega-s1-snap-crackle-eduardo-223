5ddb4190c33577e562908c40225c6dfc9a153684
Eduardo Rangel <eduardo_rp23@hotmail.com>
2021-10-18 23:40:44 -0300

    Modifica o arquivo snapCrackle.js com o que foi solicitado na atividade

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,63 @@
-// Seu código vai aqui
+function check (Value){  //Verifica se o numero e primo
+    let divisor = 0;
+    
+      for(let index = 1; index <= Value; index++){
+  
+        if(Value % index === 0){
+          divisor++;
+        }
+       }
+      if(divisor == 2){
+        return 'prime';
+  
+      }else{
+        return Value;
+      }
+  }      
+////////////////////////////////////////////////////////////////////////////////////////////
+
+function snapCracklePrime (maxValue){
+
+    let meuRetorno = [];
+
+    for(let index = 1; index <= maxValue; index++){
+        if(index % 2 === 1 && index % 5 === 0 && check(index) === 'prime'){
+          meuRetorno.push('snapCracklePrime');
+        }
+
+        else if(index % 2 === 1 && check(index) === 'prime'){
+          meuRetorno.push('snapPrime');
+        }
+
+        else if(index % 2 === 1 && index % 5 === 0 ){
+            meuRetorno.push('snapCrackle');
+        }
+
+        else if (index % 5 === 0 && check(index) === 'prime'){
+          meuRetorno.push('cracklePrime');
+        }
+
+        else if(index % 5 === 0){
+            meuRetorno.push('crackle');
+        }
+
+        else if(index % 2 === 1){
+            meuRetorno.push('snap');
+        } 
+
+        else if(check(index) === 'prime'){
+             meuRetorno.push('prime');
+        }
+
+        else{
+           meuRetorno.push(index);
+        }
+    }
+
+      return meuRetorno.join(', ');
+
+}
+
+console.log(snapCracklePrime(15));
+
+
